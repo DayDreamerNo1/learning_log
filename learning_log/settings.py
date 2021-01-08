ecc272f332ee9d3fb11ba036c8181c16bea65896
UncleZ <flynnzheng95@163.com>
2021-01-08 21:02:32 +0800

    19.1.3 finished

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -109,13 +109,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# 设置USE_TZ = False, TIME_ZONE = 'Asia/Shanghai', 则使用上海的UTC时间。
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
